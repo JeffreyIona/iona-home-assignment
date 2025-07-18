d64ff3ed4c7f4c8250a614dfe432e70830f935e7
Jeffrey Naval <jeffrey@iona.fi>
2025-07-18 21:06:51 +0800

    content

--- src/lib/contents.ts
+++ src/lib/contents.ts
@@ -2,4 +2,5 @@ export const shop = {
   title: "NovaMart",
   meta_title: "NovaMart - Style, Tech & Home Essentials in One Place",
   description: "Shop everything from skincare, fashion, and fragrances to laptops, smartphones, and home décor. NovaMart brings together curated everyday products for modern living.",
+  searchDescription: "Find exactly what you’re looking for across categories like beauty, fashion, electronics, and home essentials. Explore top products with NovaMart’s smart search.",
 };
