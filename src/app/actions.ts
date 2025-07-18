30ee717f51fbde83dc20e9dc536d72f884905a3f
Jeffrey Naval <jeffrey@iona.fi>
2025-07-18 17:14:11 +0800

    fetch with query params

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,9 +1,9 @@
 'use server'
 
 import { API_BASE_URL } from "@/lib/constants"
-import { ProductResponse } from "@/lib/types";
+import { Category, ProductResponse } from "@/lib/types";
 
-export const fetchCategories = async () => {
+export const fetchCategories = async (): Promise<Category[]> => {
   try {
     const response = await fetch(`${API_BASE_URL}/products/categories`, {
       cache: "no-store",
@@ -20,9 +20,48 @@ export const fetchCategories = async () => {
   }
 };
 
-export const fetchProducts = async ():Promise<ProductResponse> => {
+export const fetchCategoryList = async (): Promise<string[]> => {
   try {
-    const response = await fetch(`${API_BASE_URL}/products`, {
+    const response = await fetch(`${API_BASE_URL}/products/category-list`, {
+      cache: "no-store",
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch categories");
+    }
+
+    return response.json();
+  } catch (error) {
+    console.error("Error fetching categories:", error);
+    throw error;
+  }
+};
+
+export const fetchProductsByCategory = async (category: string, query?: Record<string, string>): Promise<ProductResponse> => {
+  const params = new URLSearchParams(query);
+
+  try {
+    const response = await fetch(`${API_BASE_URL}/products/category/${category}?${params.toString()}`, {
+      cache: "no-store",
+    });
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch products for category: ${category}`);
+    }
+
+    return response.json();
+
+  } catch (error) {
+    console.error(`Error fetching products for category ${category}:`, error);
+    throw error;
+  }
+};
+
+export const fetchProducts = async (query?: Record<string, string>):Promise<ProductResponse> => {
+  const params = new URLSearchParams(query);
+
+  try {
+    const response = await fetch(`${API_BASE_URL}/products?${params.toString()}`, {
       cache: "no-store",
     });
 
