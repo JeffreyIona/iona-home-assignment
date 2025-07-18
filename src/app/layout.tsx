0fb263566bc3ac46acf50fda2a5dcd4d49230d62
Jeffrey Naval <jeffrey@iona.fi>
2025-07-18 17:14:26 +0800

    fix: fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,19 @@
 import type { Metadata } from "next";
-// import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from 'next/font/google'
 import "./globals.css";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
+import { shop } from "@/lib/contents";
 
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
+const inter = Inter({
+  subsets: ['latin'],
+  variable: "--font-inter-sans",
+  display: 'swap',
+});
 
 export const metadata: Metadata = {
-  title: "NovaMart - Style, Tech & Home Essentials in One Place",
-  description: "Shop everything from skincare, fashion, and fragrances to laptops, smartphones, and home décor. NovaMart brings together curated everyday products for modern living.",
+  title: shop.meta_title,
+  description: shop.description,
 };
 
 export default function RootLayout({
@@ -27,8 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        // className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white`}
-        className={`antialiased bg-white`}
+        className={`${inter.variable} antialiased bg-white`}
       >
         <Header />
         <main>{children}</main>
