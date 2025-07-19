744e3851ae6ed4efb30d9abbb376f1265dcd0cc0
Jeffrey Naval <jeffrey@iona.fi>
2025-07-19 18:23:26 +0800

    fix header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -20,101 +20,99 @@ export default function Header() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <>
-      <header className="bg-white border-b border-b-gray-200">
-        <nav
-          aria-label="Global"
-          className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
-        >
-          <Logo />
-          <div className="flex lg:hidden gap-4">
-            <button
-              type="button"
-              onClick={() => setMobileMenuOpen(true)}
-              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+    <header className="bg-white border-b border-b-gray-200">
+      <nav
+        aria-label="Global"
+        className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+      >
+        <Logo />
+        <div className="flex lg:hidden gap-4">
+          <button
+            type="button"
+            onClick={() => setMobileMenuOpen(true)}
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+          >
+            <span className="sr-only">Open search</span>
+            <MagnifyingGlassIcon aria-hidden="true" className="size-6" />
+          </button>
+          <button
+            type="button"
+            onClick={() => setMobileMenuOpen(true)}
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+          >
+            <span className="sr-only">Open main menu</span>
+            <Bars3Icon aria-hidden="true" className="size-6" />
+          </button>
+        </div>
+        <div className="hidden lg:flex lg:flex-1 lg:max-w-1/2">
+          <Search />
+        </div>
+        <div className="hidden lg:flex lg:gap-x-12 lg:items-center">
+          {navigation.map((item) => (
+            <a
+              key={item.name}
+              href={item.href}
+              className="text-sm/6 font-semibold text-gray-900"
             >
-              <span className="sr-only">Open search</span>
-              <MagnifyingGlassIcon aria-hidden="true" className="size-6" />
-            </button>
-            <button
-              type="button"
-              onClick={() => setMobileMenuOpen(true)}
-              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-            >
-              <span className="sr-only">Open main menu</span>
-              <Bars3Icon aria-hidden="true" className="size-6" />
-            </button>
-          </div>
-          <div className="hidden lg:flex lg:flex-1 lg:max-w-1/2">
-            <Search />
-          </div>
-          <div className="hidden lg:flex lg:gap-x-12 lg:items-center">
-            {navigation.map((item) => (
-              <a
-                key={item.name}
-                href={item.href}
-                className="text-sm/6 font-semibold text-gray-900"
-              >
-                {item.name}
-              </a>
-            ))}
-            <a href="#" className="text-sm/6 font-semibold text-gray-900">
-              <ShoppingBagIcon aria-hidden="true" className="size-5" />
-              <span className="sr-only">Cart</span>
+              {item.name}
             </a>
+          ))}
+          <a href="#" className="text-sm/6 font-semibold text-gray-900">
+            <ShoppingBagIcon aria-hidden="true" className="size-5" />
+            <span className="sr-only">Cart</span>
+          </a>
+        </div>
+      </nav>
+      <Dialog
+        open={mobileMenuOpen}
+        onClose={setMobileMenuOpen}
+        className="lg:hidden"
+      >
+        <div className="fixed inset-0 z-50" />
+        <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="flex items-center justify-between">
+            <Logo />
+            <button
+              type="button"
+              onClick={() => setMobileMenuOpen(false)}
+              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+            >
+              <span className="sr-only">Close menu</span>
+              <XMarkIcon aria-hidden="true" className="size-6" />
+            </button>
           </div>
-        </nav>
-        <Dialog
-          open={mobileMenuOpen}
-          onClose={setMobileMenuOpen}
-          className="lg:hidden"
-        >
-          <div className="fixed inset-0 z-50" />
-          <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-            <div className="flex items-center justify-between">
-              <Logo />
-              <button
-                type="button"
-                onClick={() => setMobileMenuOpen(false)}
-                className="-m-2.5 rounded-md p-2.5 text-gray-700"
-              >
-                <span className="sr-only">Close menu</span>
-                <XMarkIcon aria-hidden="true" className="size-6" />
-              </button>
-            </div>
-            <div className="mt-6 flow-root">
-              <div className="-my-6 divide-y divide-gray-500/10">
-                <div className="py-6">
-                  <Search />
-                </div>
-                <div className="space-y-2 py-6">
-                  {navigation.map((item) => (
-                    <a
-                      key={item.name}
-                      href={item.href}
-                      className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
-                    >
-                      {item.name}
-                    </a>
-                  ))}
-                </div>
-                <div className="py-6">
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="py-6">
+                <Search />
+              </div>
+              <div className="space-y-2 py-6">
+                {navigation.map((item) => (
                   <a
-                    href="#"
-                    className="-mx-3 rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50 items-center flex gap-3"
+                    key={item.name}
+                    href={item.href}
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
                   >
-                    <ShoppingBagIcon
-                      aria-hidden="true"
-                      className="-mt-1 shrink-0 size-6 text-gray-400"
-                    />
-                    Cart
+                    {item.name}
                   </a>
-                </div>
+                ))}
+              </div>
+              <div className="py-6">
+                <a
+                  href="#"
+                  className="-mx-3 rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50 items-center flex gap-3"
+                >
+                  <ShoppingBagIcon
+                    aria-hidden="true"
+                    className="-mt-1 shrink-0 size-6 text-gray-400"
+                  />
+                  Cart
+                </a>
               </div>
             </div>
-          </DialogPanel>
-        </Dialog>
-      </header>
-    </>
+          </div>
+        </DialogPanel>
+      </Dialog>
+    </header>
   );
 }
